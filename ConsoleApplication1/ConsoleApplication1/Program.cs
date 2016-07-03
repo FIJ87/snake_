d8dfbf8b37e90db17b54fe2575ca1d86a0fec657
FIJ87 <fij1987@mail.ru>
2016-07-03 16:22:07 +0400

    lesson5/RAM

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -29,7 +29,44 @@ namespace ConsoleApplication1
 
             Console.ReadLine();
         }
-   
-         
+        public static void Main()
+            {
+            int x = 1;
+            Func1(x);
+            Func2(x);
+            Func3(x);
+                              
+            
+            }
+        public static void Func1(int value)
+        {
+          
+        }
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+        public static void Func3(int value)
+        {
+            x = x + 1;
+        }
+
+        public static void Main1()
+        {
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(4,5,'#');
+            Move(p1, 10, 10);
+            Reset(p2);
+        }
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y = dy;
+        }
+        public static void Reset(Point p)
+        {
+                       
+        }
     }
+    
 }
