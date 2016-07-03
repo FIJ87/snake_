3e2b0c731d3e9f46d61bbc82eaa72358b16f77bd
FIJ87 <fij1987@mail.ru>
2016-07-03 20:42:02 +0400

    lesson6/list

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,62 +11,43 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Point p1 = new Point( 1, 3, '*');
-                     
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.Draw();
 
             Point p2 = new Point( 4, 5, '#');
-          
-            
-            /* p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
 
-         
+            Point p3 = new Point(7, 8, '&');
+
+            Point p4 = new Point(6, 9, '=');
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            List<Point> pList2 = new List<Point>();
+            pList2.Add(p3);
+            pList2.Add(p4);
 
             Console.ReadLine();
         }
-        public static void Main()
-            {
-            int x = 1;
-            Func1(x);
-            Func2(x);
-            Func3(x);
-                              
-            
-            }
-        public static void Func1(int value)
-        {
-          
-        }
-        public static void Func2(int value)
-        {
-            value = value + 1;
-        }
-        public static void Func3(int value)
-        {
-            x = x + 1;
-        }
-
-        public static void Main1()
-        {
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4,5,'#');
-            Move(p1, 10, 10);
-            Reset(p2);
-        }
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y = dy;
-        }
-        public static void Reset(Point p)
-        {
-                       
-        }
+       
     }
     
 }
