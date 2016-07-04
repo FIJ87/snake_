d28c396c9b30d2191cbcf4800b29a76c6c88d248
FIJ87 <fij1987@mail.ru>
2016-07-04 23:00:15 +0400

    lesson9/paint

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,26 +10,27 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point( 1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point( 4, 5, '#');
-            p2.Draw();
-
-            Point p3 = new Point(7, 8, '&');
-
-            Point p4 = new Point(6, 9, '=');
-
+            
+                     
             horizontline upline = new horizontline(0,78,0, '+');
-            upline.Drow();
+           
             horizontline downline = new horizontline(0, 78, 24, '+');
-            downline.Drow();
+            
             Verticalline leftline = new Verticalline(0, 24, 0, '+');
-            leftline.Drow();
+           
             Verticalline rightline = new Verticalline(0, 24, 78, '+');
+            upline.Drow();
+            downline.Drow();
+            leftline.Drow();
             rightline.Drow();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
             Console.ReadLine();
         }
+        
        
     }
     
